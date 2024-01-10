a09e0f6a39fcee1384962cab4dfeace07f1519ff
ambarvargas07 <ambarjose21@gmail.com>
2024-01-09 21:43:44 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,64 @@
+
+let apelllido = "Aguilera" ;
+const DNI = 1000000;
+
+
+// Number 1,2,5,7.2
+// String "texto" , 'texto'
+
+//SUMA 
+let numero1 = 10 
+const numero2 = 5
+
+// 10 + 5
+
+let resultadoSuma = numero1 + numero2
+
+// RESTA
+
+let nuemro3 = 10
+const numero4 = 5 
+
+// 10-5 
+
+let resultadoResta = numero1 - numero2 
+
+
+
+let apodo = "Male"
+
+
+//suma de strimg y number 
+let numero5 = 5 
+let texto = "hola"
+
+let resultado = numero5 + texto
+
+// console 
+
+console.log(resultado)
+console.log(5);
+
+
+//prompt
+
+let nombre = prompt("Ingresa tu nombre")
+
+console.log(nombre)
+
+//ALERT
+
+alert("SOY UN ALERT")
+
+
+//ejemplo de actv de practica 
+
+const anioDeNacimiento = parseInt(prompt("En que año naciste?"))
+let anioActual = parseInt(prompt("Ingrese el año actual"))
+
+
+let edad = anioActual - anioDeNacimiento
+console.log(edad)
+
+alert("tu edad es: " + edad)
+
